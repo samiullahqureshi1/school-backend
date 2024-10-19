b2ddf867e5935cdefe411b15c3aea1b689253f24
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2024-10-19 00:07:11 -0700

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { dbConnection } from './connection/db_connection'
+import { dbConnection } from './connection/db_connection.js'
 import dotenv from 'dotenv'
 
 dotenv.config()
