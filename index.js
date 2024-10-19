8863f7a42639a9f651f63a587fe83eb2309ccdf2
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2024-10-19 00:09:25 -0700

    done

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ dotenv.config()
 const app=express()
 dbConnection()
 
-
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+  });
 app.use(express.json())
 app.listen(5000,()=>{
     console.log('server properly running on port 5000')
